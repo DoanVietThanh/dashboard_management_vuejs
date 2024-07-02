14b0196536a6ca40e30c95ca9cd6abddde228a72
DoanVietThanh <doanvietthanhhs@gmail.com>
2024-07-02 23:45:18 +0700

    add README.md

--- server/router/user.route.js
+++ server/router/user.route.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const { createUser, getUsers } = require('../controller/user.ctrl');
 
 const userRouter = express.Router();
+
+userRouter.post('/', createUser);
+userRouter.get('/', getUsers);
 
 module.exports = userRouter;
